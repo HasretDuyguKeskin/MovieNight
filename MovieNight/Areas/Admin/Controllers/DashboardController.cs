79a0f9ff6564cd8c5ea11033bb52d20e52c4cf09
hasret keskin <hasret_1989_@hotmail.com>
2021-04-03 17:41:15 +0300

    Add project files.

---
+++ MovieNight/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using MovieNight.Areas.Admin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNight.Areas.Admin.Controllers
+{
+    public class DashboardController : AdminBaseController
+    {
+        public object DashboardViewModel { get; private set; }
+
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            List<MovieYearCount> movieYearCounts = db.Movies
+                .GroupBy(x => x.Year)
+                .Select(g => new MovieYearCount()
+                {
+                    Year = g.Key,
+                    Count = g.Count()
+                })
+                .OrderBy(x => x.Year).ToList();
+
+            DashboardViewModel vm = new DashboardViewModel()
+            {
+                MovieYearCounts = movieYearCounts
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ MovieNight/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using MovieNight.Models;
+using MovieNight.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNight.Controllers
+{
+    public class HomeController : BaseController
+    {
+
+        public ActionResult Index(int page = 1)
+        {
+            IQueryable<Movie> query = db.Movies;
+            int totalItems = query.Count();
+            int pageSize = 9;
+            int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+            var movies = query
+                .OrderByDescending(x => x.ImdbRating)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var vm = new HomeViewModel()
+            {
+                Movies = movies,
+                PaginationInfo = new PaginationInfoViewModel()
+                {
+                    CurrentPage = page,
+                    PageSize = pageSize,
+                    ItemsOnPage = movies.Count,
+                    TotalItems = totalItems,
+                    TotalPages = totalPages,
+                    HasNext = page < totalPages,
+                    HasPrevious = page > 1
+                }
+            };
+
+            return View(vm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
